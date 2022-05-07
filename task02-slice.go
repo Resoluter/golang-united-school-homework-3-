2a35647a93c6e998cc6be75c07184ddf40bd9c5b
Sirojiddin Khujabekov <xujabekovsirojiddin@gmai.com>
2022-05-07 23:53:12 +0500

    commit

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,10 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+
+	for i := 0; i < len(input); i++ {
+		j := len(input) - 1 - i
+		result = append(result, input[j])
+	}
+	return result
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,19 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+	sortedKeys := make([]int, len(input))
+
+	for k := range input {
+		sortedKeys = append(sortedKeys, k)
+	}
+	sort.Ints(sortedKeys)
+
+	for k := range sortedKeys {
+		result = append(result, input[k])
+	}
+	return result
 }
